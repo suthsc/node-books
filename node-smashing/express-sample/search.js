ee042d0e9fcab9c9e25dfec884765a9c20eacfa0
Scott Sutherland <ssutherland@digitalriver.com>
2013-04-22 16:46:59 -0500

    Updated the .data() method to .query() for superagent.

--- node-smashing/express-sample/search.js
+++ node-smashing/express-sample/search.js
@@ -15,10 +15,9 @@ var request = require('superagent');
 
 module.exports = function search(query, fn) {
 	request.get('http://search.twitter.com/search.json')
-		.data({
-				q: query
-			})
-		.end(function(res) {
+		.query({ q: query })
+		.set('Accept', 'application/json')
+		.end(function (res) {
 			if (res.body && Array.isArray(res.body.results)) {
 				return fn(null, res.body.results);
 			}
